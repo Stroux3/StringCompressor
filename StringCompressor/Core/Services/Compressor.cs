878989f4865e1ed10f6febcb6f36d97a72ded1d6
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-18 05:33:43 +0300

    Создание класса Compressor и реализация компрессии строки

---
+++ StringCompressor/Core/Services/Compressor.cs
@@ -0,0 +1,34 @@
+﻿using StringCompressor.Core.Interfaces;
+using System.Text;
+
+namespace StringCompressor.Core.Services
+{
+    internal class Compressor : ICompressor
+    {
+        public string Compress(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            var result = new StringBuilder();
+            int count = 1;
+            for (int i = 1; i <= input.Length; i++)
+            {
+                if (i < input.Length && input[i] == input[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    result.Append(input[i - 1]);
+                    if (count > 1)
+                        result.Append(count);
+                    count = 1;
+                }
+            }
+
+            return result.ToString();
+        }
+    } 
+    
+}
